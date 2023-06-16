3e1509d4f0d03154f14f589fc81e3c92484bd5cc
syntax-co <51330734+syntax-co@users.noreply.github.com>
2023-06-16 22:22:11 +0000

    Update 2 files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,58 +1,32 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import { Fade } from 'react-slideshow-image';
-import 'react-slideshow-image/dist/styles.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Slideshow from "./component/slideshow";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-const images=[
+const images = [
   {
-    url: './images/beef.jpg',
-    caption: 'Slide 1'
+    url: "./images/beef.jpg",
+    caption: "Slide 1",
   },
   {
-    url: './images/plate-1.jpg',
-    caption: 'Slide 2'
+    url: "./images/plate-1.jpg",
+    caption: "Slide 2",
   },
   {
-    url: './images/salad-bowl.jpg',
-    caption: 'Slide 3'
+    url: "./images/salad-bowl.jpg",
+    caption: "Slide 3",
   },
   {
-    url:'./images/sushi2.jpg',
-    caption:'slide 4'
-  }
-]
-
+    url: "./images/sushi2.jpg",
+    caption: "slide 4",
+  },
+];
 
 export default function Home() {
   return (
-    <div className='hompage-body'>
-      <div className="slide-container">
-      <Fade>
-        {
-          images.map((item,index) => {
-
-            return(
-              <div key={index} className='slide-show-image-holder'>
-                <div  className='slide-show-image-body'
-                style={{
-                  backgroundPosition:'center',
-                  backgroundSize:'contain',
-                  backgroundRepeat:'no-repeat',
-                  backgroundImage:`url(${item.url})`
-                }}
-                >
-
-                </div>
-              </div>
-            )
-
-          })
-        }
-      </Fade>
-      </div>
-
+    <div className="hompage-body">
+      <Slideshow images={images} />
     </div>
-  )
+  );
 }
