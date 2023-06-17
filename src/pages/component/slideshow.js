8a1110a80cb98a050e8862930593d9b2548ca159
syntax-co <51330734+syntax-co@users.noreply.github.com>
2023-06-17 01:41:24 +0000

    Update 5 files

--- src/pages/component/slideshow.js
+++ src/pages/component/slideshow.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
+import {
+  MdOutlineArrowBackIos,
+  MdOutlineArrowForwardIos,
+} from "react-icons/md";
+import { IconContext } from "react-icons/lib";
 
 const ImageContainer = ({ image, position }) => {
   return (
@@ -15,16 +20,31 @@ const ImageContainer = ({ image, position }) => {
   );
 };
 
+const SlideshowArrows = () => {
+  return (
+    <div className="h-full w-full absolute">
+      <div className="slideshow-left-arrow" onClick={() => {}}>
+        <MdOutlineArrowBackIos size={40} />
+      </div>
+      <div className="slideshow-right-arrow" onClick={() => {}}>
+        <MdOutlineArrowForwardIos size={40} />
+      </div>
+    </div>
+  );
+};
+
 const Slideshow = ({ images }) => {
   const [layerOneImage, setLayerOneImage] = useState(0);
   const [layerTwoImage, setLayerTwoImage] = useState(1);
   const [currentLayer, setCurrentLayer] = useState(0);
 
+  const MoveShow = (direction) => {};
+
   return (
-    <div className="w-full h-2/6 ">
+    <div className="w-full h-2/6 relative">
       <motion.div
         id="item-one"
-        className="w-full h-full "
+        className="slideshow-image-layer"
         initial={{ opacity: 1 }}
         animate={{ opacity: currentLayer == 0 ? 1 : 0 }}
         style={{
@@ -34,13 +54,15 @@ const Slideshow = ({ images }) => {
 
       <motion.div
         id="item-two"
-        className="w-full h-full"
+        className="slideshow-image-layer"
         initial={{ opacity: 0 }}
         animate={{ opacity: currentLayer == 1 ? 1 : 0 }}
         style={{
           backgroundImage: `url(${images[layerTwoImage].url})`,
         }}
       ></motion.div>
+
+      <SlideshowArrows />
     </div>
   );
 };
